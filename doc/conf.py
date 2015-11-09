55ebf12ef6dddfd7a9a17d8a8ed80aa57912ec20
Matthew Harrigan <harrigan@stanford.edu>
2015-11-09 13:49:51 -0800

    Versions wip

--- doc/conf.py
+++ doc/conf.py
@@ -176,12 +176,11 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 html_sidebars = {
-    '**':[
+    '**': [
         'about.html',
         'navigation.html',
-        'relations.html',
         'searchbox.html',
-        'donate.html',
+        'versions.html',
     ]
 }
 
