4f166d9e0d8b2b8f6e9aae8bfc016ccbf69260ea
Matthew Harrigan <harrigan@stanford.edu>
2016-05-10 10:44:27 -0700

    Add SymmetryRMSDFeaturizer

--- msmbuilder/featurizer/rmsd.py
+++ msmbuilder/featurizer/rmsd.py
@@ -4,9 +4,11 @@
 # Copyright (c) 2016, Stanford University and the Authors
 # All rights reserved.
 
+import itertools
 import warnings
 
 import mdtraj as md
+import numpy as np
 
 from msmbuilder import libdistance
 from .base import Featurizer
@@ -42,8 +44,8 @@ class RMSDFeaturizer(Featurizer):
 
         self.atom_indices = atom_indices
         if self.atom_indices is not None:
-            self.sliced_reference_traj = reference_traj.atom_slice(
-                self.atom_indices)
+            self.sliced_reference_traj = \
+                reference_traj.atom_slice(self.atom_indices)
         else:
             self.sliced_reference_traj = reference_traj
 
@@ -71,7 +73,80 @@ class RMSDFeaturizer(Featurizer):
             sliced_traj = traj.atom_slice(self.atom_indices)
         else:
             sliced_traj = traj
-        result = libdistance.cdist(
-            sliced_traj, self.sliced_reference_traj, 'rmsd'
-        )
+        result = libdistance.cdist(sliced_traj,
+                                   self.sliced_reference_traj,
+                                   'rmsd')
         return result
+
+
+def rotational(x):
+    x = list(x)
+    for i in range(len(x)):
+        yield tuple(x[i:] + x[:i])
+
+
+class SymmetryRMSDFeaturizer(Featurizer):
+    """Calculate RMSD over a symmetry perturbation
+
+    Given a set of permutation groups and a function that orders those
+    groups, calculate the RMSD for each permutation and pick the lowest.
+
+    This can be used for homo-multimer proteins. For example, a homologous
+    4-domain channel can be featurized by taking the minimum RMSD over
+    the four rotations.
+
+    Parameters
+    ----------
+    reference_traj : mdtraj.Trajectory
+        The reference conformations to superpose each frame with respect to
+    chains : iterable
+        The chain indices to rotate about (using mdtraj's chain indexing)
+    atom_groups : list of array_like
+        Manually specify permutation groups by giving a list of atom
+        indices.
+    symmetry : str or callable
+        How to permute groups.
+         - rotational: start from a given group and loop around
+         - permutation: all permutations of the group
+        Callables should accept an iterable of group indices.
+    """
+
+    symmetry_types = {
+        'rotational': rotational,
+        'permutation': itertools.permutations
+    }
+
+    def __init__(self, reference_traj, chains=None, atom_groups=None,
+                 symmetry='permutation'):
+        if (chains is None) == (atom_groups is None):
+            raise ValueError("Please specify chains or atom_groups, "
+                             "but not both.")
+
+        if not callable(symmetry):
+            if symmetry not in self.symmetry_types.keys():
+                raise ValueError("symmetry must be callable or one of: {}"
+                                 .format(list(self.symmetry_types.keys())))
+            symmetry = self.symmetry_types[symmetry]
+
+        if chains is not None:
+            atom_groups = [[a.index for a in reference_traj.topology.atoms
+                            if a.residue.chain.index == chain_i]
+                           for chain_i in chains]
+
+        self.reference_traj = reference_traj
+        self.symmetry = symmetry
+        self.atom_groups = atom_groups
+
+    def partial_transform(self, traj):
+        ref_inds = np.concatenate(self.atom_groups)
+        feats = []
+        for ref_frame in range(self.reference_traj.n_frames):
+            rmsds = []
+            for perm in self.symmetry(range(len(self.atom_groups))):
+                x_inds = np.concatenate([self.atom_groups[i] for i in perm])
+                rmsds += [md.rmsd(traj, self.reference_traj, frame=ref_frame,
+                                  atom_indices=x_inds,
+                                  ref_atom_indices=ref_inds)]
+            rmsds = np.asarray(rmsds)
+            feats += [np.min(rmsds, axis=0)]
+        return np.vstack(feats).T
